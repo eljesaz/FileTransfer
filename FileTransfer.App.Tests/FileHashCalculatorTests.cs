53b1c951a3a2521477f419e806d18220e22b3e06
eljesaz <eljessa94@gmail.com>
2026-09-16 00:47:39 +0200

    Implemented the method for chunk hashing and created a unit test project where I validate the checksum logic.

---
+++ FileTransfer.App.Tests/FileHashCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+using FileTransfer.App.Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileTransfer.App.Tests;
+
+[TestClass]
+public class FileHashCalculatorTests
+{
+    [TestMethod]
+    public void ComputeChunkMd5_UsesOnlyBytesRead()
+    {
+        const string input = "hello";
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+        string expectedHash = Convert.ToHexString(
+            MD5.HashData(inputBytes));
+
+        byte[] buffer = Encoding.UTF8.GetBytes(
+            input + "-extra-data");
+
+        var calculator = new FileHashCalculator();
+
+        string actualHash = calculator.ComputeChunkMd5(
+            buffer,
+            inputBytes.Length);
+
+        Assert.AreEqual(expectedHash, actualHash);
+    }
+}
---
+++ src/FileTransfer.App/Services/FileHashCalculator.cs
@@ -0,0 +1,39 @@
+﻿namespace FileTransfer.App.Services
+{
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Creates hashes from file chunks and whole files sequentialy.
+    /// </summary>
+    public class FileHashCalculator
+    {
+        public FileHashCalculator() { }
+        public string ComputeChunkMd5(byte[] buffer, int bytesRead )
+        {
+            ArgumentNullException.ThrowIfNull(buffer);
+
+            if (bytesRead < 0 || bytesRead > buffer.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bytesRead));
+            }
+
+            byte[] hashBytes;
+
+            using (MD5 md5Hash = MD5.Create())
+            {
+                hashBytes = md5Hash.ComputeHash(
+                    buffer,
+                    0,
+                    bytesRead);
+            }
+
+            return Convert.ToHexString(hashBytes);
+        }
+        
+        public string ComputeFileSha256(string filePath)
+        {
+            return String.Empty;
+        }
+    }
+
+}
